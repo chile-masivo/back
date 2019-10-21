f78fafa5a68d89a315bcc9fc03b884092242e1de
Camilo Orellana <camilo@MacBook-Pro-de-Camilo.local>
2019-10-21 20:31:01 -0300

    entorno.

---
+++ src/server/index.js
@@ -0,0 +1,18 @@
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+// Creates an ExpressJS compatible Feathers application
+const app = express(feathers());
+
+// Parse HTTP JSON bodies
+app.use(express.json());
+// Parse URL-encoded params
+app.use(express.urlencoded({ extended: true }));
+// Host static files from the current folder
+app.use(express.static(__dirname));
+
+
+
+// Start the server
+app.listen(3030).on('listening', () =>
+  console.log('Feathers server listening on localhost:3030')
+);
